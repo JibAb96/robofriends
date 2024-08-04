e155cb5a6831aded0010ee7046b726c42215a0ba
JibAb96 <j.abshir96@gmail.com>
2024-08-04 08:40:16 +0000

    Display all robofriends

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Cards from "./Cards.js"
+import CardLists from './CardsList.js';
 import "tachyons"
 import {robots} from "./robots.js"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Cards id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Cards id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Cards id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    <CardLists robots={robots}/>
   </React.StrictMode>
 );
 
