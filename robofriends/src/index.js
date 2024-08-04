406ca79a026312632e39922a1865e512487341d2
JibAb96 <j.abshir96@gmail.com>
2024-08-04 09:15:16 +0000

    Search box made interactive

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import CardLists from './CardsList.js';
 import "tachyons"
-import {robots} from "./robots.js"
+import App from "./App.js"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardLists robots={robots}/>
+    <App/>
   </React.StrictMode>
 );
 
